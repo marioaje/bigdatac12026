32f22b32da03ade165950f7a86f9545ac81bb07d
marioaje <marioaje@gmail.com>
2026-03-23 09:57:48 -0600

    Update muestra.py
    Analinzando los precios uy productos

--- Semana 11/muestra.py
+++ Semana 11/muestra.py
@@ -20,3 +20,70 @@ print(soup.title.text)
 #encabezado
 print("\nh1") #\n salto de linea es equivalente a dar un enter
 print(soup.find("h1").text)
+
+print("\nh2") #\n salto de linea es equivalente a dar un enter
+print(soup.find("h2").text)
+
+
+
+print("\nProductos y h3") #\n salto de linea es equivalente a dar un enter
+listaproductos = soup.find_all("div", class_="producto")
+
+for itmeproducto in listaproductos:
+    nombre = itmeproducto.find("h3").text
+    precio = itmeproducto.find("p").text
+    print("\nNombre: ", nombre, " precio: ", precio)
+
+# print(soup.find("h3").text)
+
+
+    
+
+#     <p class="descripcion">Las mejores galletas del país.</p>
+
+    
+
+#     <div class="producto">
+#         <h3>Galleta Chocolate</h3>
+#         <p class="precio">Precio: 1200</p>
+#     </div>
+
+#     <div class="producto">
+#         <h3>Galleta Vainilla</h3>
+#         <p class="precio">Precio: 1000</p>
+#     </div>
+
+#     <div class="producto">
+#         <h3>Galleta Fresa</h3>
+#         <p class="precio">Precio: 1100</p>
+#     </div>
+
+#     <h2>Opiniones</h2>
+
+#     <ul>
+#         <li class="opinion">Muy ricas</li>
+#         <li class="opinion">Excelente sabor</li>
+#         <li class="opinion">No me gustaron</li>
+#     </ul>
+
+#     <h2>Tabla de información</h2>
+
+#     <table border="1">
+#         <tbody><tr>
+#             <th>Producto</th>
+#             <th>Calorías</th>
+#         </tr>
+#         <tr>
+#             <td>Chocolate</td>
+#             <td>250</td>
+#         </tr>
+#         <tr>
+#             <td>Vainilla</td>
+#             <td>200</td>
+#         </tr>
+#     </tbody></table>
+
+#     <a href="https://example.com">Visitar tienda</a>
+
+
+# </body>
