06b15ed52290bb601296a505d95296f8cfa924e4
marioaje <marioaje@gmail.com>
2026-03-23 09:33:23 -0600

    Update muestra.py
    Revision de etqieutas

--- Semana 11/muestra.py
+++ Semana 11/muestra.py
@@ -9,3 +9,14 @@ respuesta = requests.get(url)
 
 #doble check, verifica si la pagina tiene informacion y se cargo
 print("Estado: ", respuesta.status_code )
+
+#conversion html a un objeto
+soup = BeautifulSoup(respuesta.text, "html.parser")
+
+#Leer lo que tiene la pagina 
+print("\nTitulo") #\n salto de linea es equivalente a dar un enter
+print(soup.title.text)
+
+#encabezado
+print("\nh1") #\n salto de linea es equivalente a dar un enter
+print(soup.find("h1").text)
